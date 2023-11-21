7561f3dab36bdd8668bf0807be206ac4f44eda1a
Aman_Sachdeva <sachdevaaman434@gmail.com>
2023-11-21 23:53:04 +0530

    58. Length of Last Word

---
+++ 58-Length-of-Last-Word/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+      int len=0;
+      int i=s.length()-1;
+      while(s.charAt(i)==' '){
+          i--;
+      }  
+      while(i>=0 && s.charAt(i)!= ' '){
+          len++;
+          i--;
+      }
+      return len;
+    }
+}
